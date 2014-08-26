10687c39bcafa04985bf4fee3a620e3f20719a15
Ian Knox <knoxid@gmail.com>
2014-08-26 09:31:48 -0600

    refactored jquery objects

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,13 +44,14 @@ $(document).ready(function () {
       }
     ]
   };
-  $.each(gschoolDogs.dogs, function (i, dog) {
-  $('tbody').append("<tr class='" + dog.owner + "'></tr>")
-    $('.' + dog.owner + '').append("<td>"+ dog.owner + "</td>")
-    $('.' + dog.owner + '').append("<td>"+ dog.name + "</td>")
-    $('.' + dog.owner + '').append("<td>"+ dog.preferences[0].favoriteFood + "</td>")
-    $('.' + dog.owner + '').append("<td>"+ dog.preferences[1].favoriteToy + "</td>")
 
-  })
+  $.each(gschoolDogs.dogs, function (i, dog) {
+  $('tbody').append("<tr class='" + dog.owner + "'></tr>");
+    $('.' + dog.owner).append("<td>" + dog.owner + "</td>");
+    $('.' + dog.owner).append("<td>" + dog.name + "</td>");
+    $('.' + dog.owner).append("<td>" + dog.preferences[0].favoriteFood + "</td>");
+    $('.' + dog.owner).append("<td>" + dog.preferences[1].favoriteToy + "</td>");
+
+  });
 });
 
